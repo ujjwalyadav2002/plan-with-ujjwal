a4f9274416b0de91c372386e6369703e72187e57
ujjwalyadav2002 <ujjwalyadav2024.com>
2023-07-14 00:30:48 +0530

    deploy

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import data from './data';
+import Tours from './componenets/Tours';
+
+const App = () => {
+  const [tours,setTours]=useState(data);
+  function removeTour(id){
+    const newTours=tours.filter(tours =>tours.id !== id);
+      setTours(newTours);
+  }
+if(tours.length==0){
+  return (
+    <div className="refresh">
+      <h2>No Tours Left</h2>
+        <button className="btn-white " onClick={()=>setTours(data)}>Refresh</button>
+      
+    </div>
+  )
+}
+  return (
+    <div>
+     
+      <Tours tours={tours} removeTour={removeTour}></Tours>
+
+    </div>
+  )
+};
+
+export default App;
